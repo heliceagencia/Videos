4d6a5f59274cc1c5eb4c6d58e30d6fd17c979cb1
Ezequiel <ezepuglisi@gmail.com>
2022-12-22 02:08:04 -0300

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import ReactPlayer from 'react-player'
 import { Box, Center } from '@chakra-ui/react'
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import AdnComponent from './AdnComponent'
 
 function App() {
 
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/adn" element={<AdnComponent />} />
       </Routes>
-    </Router>
+    </HashRouter>
   )
 }
 
