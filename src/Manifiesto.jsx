2d5df6dade46e257e568a359c1537342a76b5874
Ezequiel <ezepuglisi@gmail.com>
2022-12-23 00:25:50 -0300

    change link

--- src/Manifiesto.jsx
+++ src/Manifiesto.jsx
@@ -7,7 +7,7 @@ import img from './assets/movete-8.png'
 const Manifiesto = () => {
     return (
         <Center id='particles-js' backgroundImage={img} height={'98vh'} margin-top={'auto'}>
-            <ReactPlayer style={{ 'margin': 'auto' }} width={'90%'} height={'90%'} url='https://youtu.be/9Iyj0LIadGg' controls />
+            <ReactPlayer style={{ 'margin': 'auto' }} width={'90%'} height={'90%'} url='https://www.twitch.tv/helice_agencia' controls />
         </Center>
     )
 }
