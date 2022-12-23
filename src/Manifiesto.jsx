a1ce8aa0066997fd254d208dcf49cc6f5a3038d1
Ezequiel <ezepuglisi@gmail.com>
2022-12-22 22:44:41 -0300

    algo

--- src/Manifiesto.jsx
+++ src/Manifiesto.jsx
@@ -7,7 +7,7 @@ import img from './assets/movete-8.png'
 const Manifiesto = () => {
     return (
         <Center id='particles-js' backgroundImage={img} height={'98vh'} margin-top={'auto'}>
-            <ReactPlayer style={{ 'margin': 'auto' }} width={'90%'} height={'90%'} url='https://youtu.be/wUVuqECgS44' controls />
+            <ReactPlayer style={{ 'margin': 'auto' }} width={'90%'} height={'90%'} url='https://youtu.be/SKu5LY88vmY' controls />
         </Center>
     )
 }
